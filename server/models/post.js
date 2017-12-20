079de3efcfe97e63a9d7b321b9f5675df495f4d3
JCOMMERCE\patk <pawel.tkocz@jcommerce.pl>
2017-12-20 21:05:47 +0100

    Add Read and Create posts

---
+++ server/models/post.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: String,
+  description: String
+});
+
+var Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Post = require("../models/post");
+
+mongoose.connect('mongodb://localhost:27017/posts');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Connection error"));
+db.once('open', (callback) => {
+  console.log("Connection succeeded!");
+});
 
 const app = express();
 
@@ -10,15 +20,39 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/posts", (req, res) => {
-  res.send([
-    {
-      title : "Hello World",
-      description: "Hi there! How are you?"
+  Post.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
     }
-  ])
+
+    res.send({posts: data});
+  }).sort({_id: -1});
 });
 
+app.post("/posts", (req, res) => {
+  const db = req.db;
+  const title = req.body.title;
+  const description = req.body.description;
+  
+  const new_post = new Post({
+    title: title,
+    description: description
+  });
 
+  new_post.save((err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.send({
+      success: true,
+      message: "Post was created!"
+    })
+  })
+
+})
 
 
 app.listen(process.env.PORT || 3000 );
